369fcaa8a39e1322a99ef2bcc91e1d451ee6b786
Zachary Michniewicz <33406083+ZachM1ch@users.noreply.github.com>
2023-04-11 15:21:51 -0400

    Add files via upload

--- MTG-DeckTool.py
+++ MTG-DeckTool.py
@@ -1,7 +1,7 @@
 ##---------------------##
 ## Zachary Michniewicz ##
-##    DeckTool V0.5    ##
-##  14 February 2023   ##
+##    DeckTool V0.8    ##
+##    11 April 2023    ##
 ##---------------------##
 
 # imports
@@ -11,6 +11,9 @@ import PIL.Image
 import PIL.ImageTk
 from io import BytesIO
 from tkinter import *
+import webbrowser
+import smtplib
+from email.mime.text import MIMEText
 
 # create root window
 root = Tk()
@@ -37,35 +40,52 @@ decklist = []
 # preparing variable for decklist viewing
 title_label = Text(root, height= 1, borderwidth=0)
 
+# function to open TCGPlayer mass entry page
+def tcg():
+    webbrowser.open('https://www.tcgplayer.com/massentry?productline=Magic')
+    
+# function to check if card is legal in selected format
+def formatted():
+    # get radio button value
+    rad = radio.get()
+    return rad
+    
+
 # function to add cards to decklist
 def added(index):
+
+    #check if card is legal in selected format    
+    if str(formatted()) == 'kitchen' or call_result['data'][index]['legalities'][str(formatted())] == 'legal' or call_result['data'][index]['legalities'][str(formatted())] == 'restricted':
+        error_label.configure(text = "")
     
-    # increase decklist qty if card is on already
-    same = False
-    for i in decklist:
-        if i['name'] == call_result['data'][index]['name']:
-            i['qty'] += int(add_var[index].get())
-            same = True
-            if i['qty'] < 1:
-                decklist.remove(i)
-            break;
-    if same == False:
-        decklist.append({'qty': int(add_var[index].get()), 'name': call_result['data'][index]['name'], 'set': call_result['data'][index]['set']})
+        # increase decklist qty if card is on already
+        same = False
+        for i in decklist:
+            if i['name'] == call_result['data'][index]['name']:
+                i['qty'] += int(add_var[index].get())
+                same = True
+                if i['qty'] < 1:
+                    decklist.remove(i)
+                    break;
+        if same == False:
+            decklist.append({'qty': int(add_var[index].get()), 'name': call_result['data'][index]['name'], 'set': call_result['data'][index]['set']})
     
-        # format set code
-        cap_set = '['
-        for i in decklist[-1]['set']:
-            if i.isalpha() == True:
-                cap_set += i.capitalize()
-            else:
-                cap_set += i
-        cap_set += ']'
-        decklist[-1]['set'] = cap_set
+            # format set code
+            cap_set = '['
+            for i in decklist[-1]['set']:
+                if i.isalpha() == True:
+                    cap_set += i.capitalize()
+                else:
+                    cap_set += i
+            cap_set += ']'
+            decklist[-1]['set'] = cap_set
     
-    entries[index].delete(0, END)
+        entries[index].delete(0, END)
     
-    same == False
-    print(decklist)   
+        same == False
+        print(decklist)  
+    else:
+         error_label.configure(text = "The card chosen is not legal in the selected format", fg = 'red')
 
 # function to view decklist
 def viewed():
@@ -88,7 +108,49 @@ def viewed():
     
 # function to save decklist
 def saved():
-    g = 1    
+    # reset everything
+    resetLists(labels, images, entries, buttons)
+    title_label.delete("1.0", "end")
+    title_label.grid_remove()
+    
+    #retrieve decklist name
+    dln = txt_list.get()
+    if dln == "":
+        dln = "Decklist"
+    
+    # get data from viewable list
+    f = open(dln + ".txt", "w")
+    for i in decklist:
+        f.write(str(i['qty']) + " " + i['name'] + " " + i['set'] + "\n")
+    f.close()
+    
+# function to send list via email
+def sent():
+    # variable for body text
+    body = ""
+    for i in decklist:
+        body += str(i['qty']) + " " + i['name'] + " " + i['set'] + "\n"
+    
+    #save deck automatically
+    saved()
+    
+    #retrieve decklist name
+    dln = txt_list.get()
+    if dln == "":
+        dln = "Decklist"
+    
+    #https://stackoverflow.com/questions/6270782/how-to-send-an-email-with-python
+        
+    #### Get mailgun api in late april
+    
+   #def send_simple_message():
+       #return requests.post(
+       #"https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages",
+       #auth=("api", "YOUR_API_KEY"),
+       #data={"from": "MTG DeckTool <mailgun@YOUR_DOMAIN_NAME>",
+       #      "to": [emailname.get(), emailname.get()],
+       #      "subject": dln,
+       #      "text": body})
 
 # function to format search term for api call
 def searchFormat(term):
@@ -126,7 +188,7 @@ def searched():
     resetLists(labels, images, entries, buttons)
     title_label.delete("1.0", "end")
     title_label.grid_remove()
-    
+        
     # api call
     req = "https://api.scryfall.com/cards/search?q="
     tmp_res = txt.get()
@@ -134,78 +196,127 @@ def searched():
     full_req = requests.get(reqs)
     full_list = json.loads(full_req.text)
     
-    call_result = full_list
-    
-    # determine list of displayed results
-    list_len = len(full_list['data'])
-    if list_len < 16:
-        range_run = list_len
+    if full_list['object'] == 'error':
+        error_label.configure(text = "The search term entered did not return any results", fg = 'red')
     else:
-        range_run = 16
+        error_label.configure(text = "")
         
-    # create empty labels across window
-    row_ct = 1
-    for i in range(range_run):
-        labels.append(Label(root, text = "", justify = "left", anchor = "nw", width = 25, wraplength = 150))
-        entries.append(Entry(root, justify = "right", textvariable = add_var[i], width = 10))
-        buttons.append(Button(root, text = "Add to deck", command=lambda index=i: added(index)))
-        col = ((int((i)/4)+1)*3)-2
-        images.append(Label(root))
-        images[i].grid(column =col, row =row_ct, rowspan=5)
-        labels[i].grid(column =col+1, row =row_ct, columnspan=2, rowspan=5)
-        row_ct += 5
-        entries[i].grid(column =col+1, row = row_ct, sticky="e", padx=7)
-        buttons[i].grid(column =col+2, row = row_ct, sticky="w")
-        row_ct += 1
-        
-        if row_ct > 24:
-            row_ct = 1
+        call_result = full_list
     
-    # gather and format the results of the api call for card
-    card_list = ""
-    for i in range(range_run):   
-        card_list = card_list + "------------------------------\n"
-        card_list = card_list + "Name: " + full_list['data'][i]['name'] + "\n"
-        card_list = card_list + "Set: " + full_list['data'][i]['set_name'] + "\n"
-        card_list = card_list + "CMC: " + str(int(full_list['data'][i]['cmc'])) + "\n"
-        card_list = card_list + "Price: $" + str(full_list['data'][i]['prices']['usd']) + "\n"
-        
-        # get image of card
-        img_url = full_list['data'][i]['image_uris']['small']
-        img_resp = requests.get(img_url)
-        img_actual = PIL.Image.open(BytesIO(img_resp.content))
-        real_img = PIL.ImageTk.PhotoImage(img_actual)
-        
-        # configure card label to image
-        images[i].configure(image = real_img)
-        images[i].image = real_img
-
-        # get and format more rresults from card (for format legality)
-        legal = full_list['data'][i]['legalities'].items()
-        for k, v in legal:
-            if (k == 'standard' or k == 'modern' or k == 'legacy' or k == 'vintage' or k == 'commander'):
-                card_list = card_list + k + ": " + v + "\n"
+        # determine list of displayed results
+        list_len = len(full_list['data'])
+        if list_len < 16:
+            range_run = list_len
+        else:
+            range_run = 16
             
-        card_list = card_list + "------------------------------"
-        
-        # configure text label to formatted results
-        labels[i].configure(text = card_list)
-        
-        # reset formatted text var for next card
+        # create empty labels across window
+        row_ct = 1
+        for i in range(range_run):
+            labels.append(Label(root, text = "", justify = "left", anchor = "nw", width = 25, wraplength = 150))
+            entries.append(Entry(root, justify = "right", textvariable = add_var[i], width = 10))
+            buttons.append(Button(root, text = "Add to deck", command=lambda index=i: added(index)))
+            col = ((int((i)/4)+1)*3)-2
+            images.append(Label(root))
+            images[i].grid(column =col, row =row_ct, rowspan=5)
+            labels[i].grid(column =col+1, row =row_ct, columnspan=2, rowspan=5)
+            row_ct += 5
+            entries[i].grid(column =col+1, row = row_ct, sticky="e", padx=7)
+            buttons[i].grid(column =col+2, row = row_ct, sticky="w")
+            row_ct += 1
+            
+            if row_ct > 24:
+                row_ct = 1
+                
+        # gather and format the results of the api call for card
         card_list = ""
-
-
+        for i in range(range_run):   
+            card_list = card_list + "------------------------------\n"
+            card_list = card_list + "Name: " + full_list['data'][i]['name'] + "\n"
+            card_list = card_list + "Set: " + full_list['data'][i]['set_name'] + "\n"
+            card_list = card_list + "CMC: " + str(int(full_list['data'][i]['cmc'])) + "\n"
+            card_list = card_list + "Price: $" + str(full_list['data'][i]['prices']['usd']) + "\n"
+            
+            # get image of card
+            img_url = full_list['data'][i]['image_uris']['small']
+            img_resp = requests.get(img_url)
+            img_actual = PIL.Image.open(BytesIO(img_resp.content))
+            real_img = PIL.ImageTk.PhotoImage(img_actual)
+            
+            # configure card label to image
+            images[i].configure(image = real_img)
+            images[i].image = real_img
+            
+            # get and format more rresults from card (for format legality)
+            legal = full_list['data'][i]['legalities'].items()
+            for k, v in legal:
+                if (k == 'standard' or k == 'modern' or k == 'legacy' or k == 'vintage' or k == 'commander'):
+                    card_list = card_list + k + ": " + v + "\n"
+                    
+            card_list = card_list + "------------------------------"
+                    
+            # configure text label to formatted results
+            labels[i].configure(text = card_list)
+                    
+            # reset formatted text var for next card
+            card_list = ""
+                    
+                    
 # left column of buttons
-leftside = Label(root, text = "Column", justify = "left", anchor = "nw")
+leftside = Label(root, text = "", justify = "left", anchor = "nw")
 leftside.grid(column=0, row=0)
 
 # view deck button
 view_deck = Button(root, text = "View Deck", command=viewed)
 view_deck.grid(column=0, row=1)
 
+# left column of buttons
+listname = Label(root, text = "List Name:", justify = "left", anchor = "nw")
+listname.grid(column=0, row=2)
+
+# deck name field
+txt_list = Entry(root, width=15)
+txt_list.grid(column =0, row =3) 
+
 # save deck button
 save_deck = Button(root, text = "Save Deck", command=saved)
-save_deck.grid(column=0, row=2)
+save_deck.grid(column=0, row=4)
+
+# tcgplayer button
+tcg_entry = Button(root, text = "Entry Page", command=tcg)
+tcg_entry.grid(column=0, row=5)
+
+# email
+email_label = Label(root, text = "Your Email:", justify = "left", anchor = "nw")
+email_label.grid(column=0, row=7)
+
+# email entry field
+emailname = Entry(root, width=15)
+emailname.grid(column =0, row =8) 
+
+# send list button
+send_list = Button(root, text = "Send List", command=sent)
+send_list.grid(column=0, row=9)
+
+# radio label
+radio_label = Label(root, text = "Legality:", justify = "left", anchor = "nw")
+radio_label.grid(column=0, row=10)
+
+# radio buttons
+radio = StringVar(root, "kitchen")
+R1 = Radiobutton(root, text="Kitchen Table", variable=radio, value="kitchen", command=formatted)
+R1.grid(column = 0, row = 11, sticky = 'w')
+R1 = Radiobutton(root, text="Standard", variable=radio, value="standard", command=formatted)
+R1.grid(column = 0, row = 12, sticky = 'w')
+R1 = Radiobutton(root, text="Modern", variable=radio, value="modern", command=formatted)
+R1.grid(column = 0, row = 13, sticky = 'w')
+R1 = Radiobutton(root, text="Legacy", variable=radio, value="legacy", command=formatted)
+R1.grid(column = 0, row = 14, sticky = 'w')
+R1 = Radiobutton(root, text="Vintage", variable=radio, value="vintage", command=formatted)
+R1.grid(column = 0, row = 15, sticky = 'w')
+R1 = Radiobutton(root, text="Commander", variable=radio, value="commander", command=formatted)
+R1.grid(column = 0, row = 16, sticky = 'w')
+
 
 # labels along the top
 search_term = Label(root, text = "Enter your search", justify = "left", anchor = "nw")
@@ -218,6 +329,10 @@ txt.grid(column =2, row =0)
 # button widget for search bar
 btn = Button(root, text = "Search", command=searched)
 btn.grid(column=3, row=0, padx=20)
+
+# label for potential errors
+error_label = Label(root, text = "")
+error_label.grid(column = 4, row = 0)
        
  
 # Execute Tkinter
